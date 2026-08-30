019fa2ae8a33caf8e8a3c4f049173ab4ff125f70
Shenal <shenal.bimalsha@gmail.com>
2026-08-30 21:39:30 +0530

    Add Dealer model class representing spare parts dealer information

--- src/main/java/lk/cm1601/malabe_spare_system/model/CartItem.java
+++ src/main/java/lk/cm1601/malabe_spare_system/model/CartItem.java
@@ -1,4 +1,38 @@
 package lk.cm1601.malabe_spare_system.model;
 
 public class CartItem {
+
+    private Part part;
+    private int quantity;
+    private double subtotal;
+
+    public CartItem(Part part, int quantity, double subtotal) {
+        this.part = part;
+        this.quantity = quantity;
+        this.subtotal = subtotal;
+    }
+
+    public Part getPart() {
+        return part;
+    }
+
+    public void setPart(Part part) {
+        this.part = part;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(double subtotal) {
+        this.subtotal = subtotal;
+    }
 }
